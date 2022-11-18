7948193c0adb993e353aa1090b245d0100eed141
nikoldan <nikoldan@zhaw.ch>
2022-11-18 14:57:06 +0100

    model - kunde vorbereitet

--- createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/model/Kunde.java
+++ createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/model/Kunde.java
@@ -1,18 +1,28 @@
 package ch.zhaw.createyourgarden.createyourgarden.model;
 
-
 import org.springframework.data.annotation.Id;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @RequiredArgsConstructor
 @Getter
+@Setter
+@Document("kunde")
 public class Kunde {
+    @Id
+    private String id;
+    @NonNull
+    private String vorname;
+    @NonNull
+    private String name;
+    @NonNull
+    private int plz;
+    @NonNull
+    private String ort;   
 
-
-    
 }
---
+++ createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/model/KundeCreateDTO.java
@@ -0,0 +1,13 @@
+package ch.zhaw.createyourgarden.createyourgarden.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class KundeCreateDTO {
+    private String vorname;
+    private String name;
+    private int plz;
+    private String ort;
+}
