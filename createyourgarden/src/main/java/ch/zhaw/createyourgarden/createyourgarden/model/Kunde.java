2bc8d7afcab0dbe82dbf46266f1c3bf85da0d4d6
nikoldan <nikoldan@zhaw.ch>
2022-11-18 14:30:56 +0100

    Closes #5

---
+++ createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/model/Kunde.java
@@ -0,0 +1,18 @@
+package ch.zhaw.createyourgarden.createyourgarden.model;
+
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Getter
+public class Kunde {
+
+
+    
+}
