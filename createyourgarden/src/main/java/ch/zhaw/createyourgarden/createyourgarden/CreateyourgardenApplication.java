ef1a7c22f2b3e571124b3f76880d78969b17e553
nikoldan <nikoldan@zhaw.ch>
2022-11-18 12:13:29 +0100

    first commit - create Spring Boot Web Projekt

---
+++ createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/CreateyourgardenApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.createyourgarden.createyourgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateyourgardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateyourgardenApplication.class, args);
+	}
+
+}
---
+++ createyourgarden/src/test/java/ch/zhaw/createyourgarden/createyourgarden/CreateyourgardenApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.createyourgarden.createyourgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateyourgardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
