1ea1f61147823bc07cc3dc6fff1513b878d8effc
nikoldan <nikoldan@zhaw.ch>
2022-11-28 10:26:52 +0100

    #6 Frontend vorbereitet und eingebettet

---
+++ createyourgarden/freelancer4u-frontend/src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Router from "svelte-spa-router";
+	import routes from "./routes";
+</script>
+
+<div id="app">
+	<nav class="navbar navbar-expand-lg bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#/">Home</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarNav"
+				aria-controls="navbarNav"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon" />
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a
+							class="nav-link"
+							aria-current="page"
+							href="#/artikel">Pflanzen</a
+						>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#/bestellung">Bestellungen</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="container">
+		<Router {routes} />
+	</div>
+</div>
