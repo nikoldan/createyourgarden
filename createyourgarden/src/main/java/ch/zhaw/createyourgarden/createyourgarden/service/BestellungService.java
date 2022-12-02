0414dce485f19576cd07aa03d2352adede00148e
nikoldan <nikoldan@zhaw.ch>
2022-12-02 12:44:45 +0100

    #20 Zwischenstand BestllungService

--- createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/service/BestellungService.java
+++ createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/service/BestellungService.java
@@ -1,6 +1,15 @@
 package ch.zhaw.createyourgarden.createyourgarden.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ch.zhaw.createyourgarden.createyourgarden.repository.BestellungRepository;
+
+@Service
 public class BestellungService {
+    @Autowired
+    BestellungRepository bestellungRepository;
+
     
-    
+
 }
