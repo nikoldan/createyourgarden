9c16f1241712cd17949bfe884d092176781a1492
nikoldan <nikoldan@zhaw.ch>
2022-12-02 13:22:42 +0100

    #20 bestellungBestaetigenDTO vorbereitet

---
+++ createyourgarden/src/main/java/ch/zhaw/createyourgarden/createyourgarden/model/bestellungBestaetigenDTO.java
@@ -0,0 +1,10 @@
+package ch.zhaw.createyourgarden.createyourgarden.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class bestellungBestaetigenDTO {
+    private String bestellungId;
+}
