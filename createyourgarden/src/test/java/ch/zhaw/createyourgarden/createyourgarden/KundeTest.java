c0afff5ff4f9a0c5046b58785371f9d518165590
nikoldan <nikoldan@zhaw.ch>
2022-12-13 19:09:40 +0100

    #60 Zwischenstand

--- createyourgarden/src/test/java/ch/zhaw/createyourgarden/createyourgarden/KundeTest.java
+++ createyourgarden/src/test/java/ch/zhaw/createyourgarden/createyourgarden/KundeTest.java
@@ -1,5 +1,17 @@
 package ch.zhaw.createyourgarden.createyourgarden;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import org.junit.jupiter.api.Test;
+
+import ch.zhaw.createyourgarden.createyourgarden.model.Kunde;
+
 public class KundeTest {
     
+    @Test
+    public void testNewKunde() {
+        Kunde newKunde = new Kunde("testName", "testEmail");
+        assertEquals("testName", newKunde.getName());
+        assertNotEquals(newKunde.getName(), newKunde.getEmail());
+    }
 }
--- createyourgarden/src/test/java/ch/zhaw/createyourgarden/createyourgarden/ServiceControllerTest.java
+++ createyourgarden/src/test/java/ch/zhaw/createyourgarden/createyourgarden/ServiceControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
